91f0144bfa355335a9a1243231dc4acfec8fa90e
Jonathan Ostertag <jo.ostertag@tum.de>
2021-12-07 10:01:20 +0100

    Day7

--- src/Day7.java
+++ src/Day7.java
@@ -4,11 +4,11 @@ import java.util.stream.Collectors;
 
 public class Day7 extends DAY {
     public static void main(String[] args) {
-        List<Integer> input = Arrays.stream(readFromResFile("Day7.txt").get(0).split(","))
+        List<Integer> input = Arrays.stream(readFromResFile("Day7-test.txt").get(0).split(","))
                 .map((String s) -> Integer.parseInt(s.replaceAll(" ", "")))
                 .collect(Collectors.toList());
 
-        long avg = Math.round(input.stream().mapToInt((Integer n) -> (int) n).average().orElse(0));
+        long avg = Math.round(input.stream().mapToInt((Integer n) -> (int) n).average().orElse(0)-0.1);
 
         long fuel = 0;
         for (int n : input) {
